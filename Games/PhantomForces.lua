8b1500e0eda9077bfec1217fb2d1db92291d859d
Jaiden2chill <118320943+Jaiden2chill@users.noreply.github.com>
2022-11-15 19:03:21 -0600

    Add files via upload

---
+++ Games/PhantomForces.lua
@@ -0,0 +1,2 @@
+game.Players.LocalPlayer:Kick("Vestra | Game Is Currently Patched Due To A FrameWork Update | Join Discord To Be Notified When Its Up")
+setclipboard("https://discord.gg/eWrT8m9h8x")
